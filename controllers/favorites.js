01b8836c310670d277758c853b982f9e78c604f0
sendistephen <sendi.steve2@gmail.com>
2021-10-15 14:19:39 +0300

    Add favorites logic

---
+++ controllers/favorites.js
@@ -0,0 +1,47 @@
+const Favorites = require('../model/Favorites');
+
+exports.savePhoto = async (req, res) => {
+  // check if photo exists
+  let foundPhoto = await Favorites.findOne({ id: req.body.id });
+
+  if (foundPhoto) {
+    //  photo exists
+    return res.status(400).json({ error: 'Photo exists already' });
+  }
+  // save photo
+  foundPhoto = new Favorites({
+    id: req.body.id,
+    description: req.body.description,
+    alt_description: req.body.alt_description,
+    created_at: req.body.created_at,
+    updated_at: req.body.updated_at,
+    urls: req.body.urls,
+    user: req.body.user,
+    user_id: req.user.sub,
+  });
+
+  await foundPhoto.save((err, result) => {
+    if (err) {
+      return res.status(400).json({ error: 'Photo not saved' });
+    }
+    return res.json({ success: true, result });
+  });
+};
+exports.getAllSavedPhotos = async (req, res) => {
+  const data = await Favorites.find({ user_id: req.user.sub });
+  if (!data) {
+    return res.status(400).json({ msg: 'No data found' });
+  }
+  return res.json(data);
+};
+// remove favorite photo
+exports.deletePhoto = async (req, res) => {
+  const foundPhoto = await Favorites.findOneAndRemove({
+    id: req.params.id,
+    user_id: req.user.sub,
+  });
+  if (!foundPhoto) {
+    return res.status(400).json({ msg: 'Resource not found!' });
+  }
+  return res.json({ id: foundPhoto.id });
+};
