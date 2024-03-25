ce66b93fed576ca7f994b79ec9466433ceb497c4
sendistephen <sendi.stev@gmail.com>
2024-03-25 14:24:01 +0300

    Refactor savePhoto function for improved error handling and consistency

--- controllers/favorites.js
+++ controllers/favorites.js
@@ -1,33 +1,31 @@
 import Favorites from '../model/Favorites.js';
 
 export async function savePhoto(req, res) {
-  console.log(req.headers);
-  // check if photo exists
-  let foundPhoto = await Favorites.findOne({ id: req.body.id });
+  try {
+    let foundPhoto = await Favorites.findOne({ id: req.body.id });
 
-  if (foundPhoto) {
-    //  photo exists
-    return res.status(400).json({ error: 'Photo exists already' });
-  }
-  // save photo
-  foundPhoto = new Favorites({
-    id: req.body.id,
-    description: req.body.description,
-    alt_description: req.body.alt_description,
-    created_at: req.body.created_at,
-    updated_at: req.body.updated_at,
-    urls: req.body.urls,
-    user: req.body.user,
-    user_id: req.user.sub,
-  });
-
-  await foundPhoto.save((err, result) => {
-    if (err) {
-      return res.status(400).json({ error: 'Photo not saved' });
+    if (foundPhoto) {
+      return res.status(400).json({ error: 'Photo exists already' });
     }
-    return res.json({ success: true, result });
-  });
+    const newFavoritePhoto = new Favorites({
+      id: req.body.id,
+      description: req.body.description,
+      alt_description: req.body.alt_description,
+      created_at: req.body.created_at,
+      updated_at: req.body.updated_at,
+      urls: req.body.urls,
+      user: req.body.user,
+      user_id: req.user.sub,
+    });
+    const savedPhoto = await newFavoritePhoto.save();
+    return res.json({ success: true, photo: savedPhoto });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: 'An internal server error occurred.' });
+  }
 }
+
 export async function getAllSavedPhotos(req, res) {
   const data = await Favorites.find({ user_id: req.user.sub });
   if (!data) {
